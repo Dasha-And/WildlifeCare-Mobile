6c066ffe1398772497eb0bb4d0cfbbb6388d56c7
Dasha-And <dosados69@gmail.com>
2021-12-11 00:28:28 +0200

    maps

--- app/src/main/java/com/example/wildifecare_mobile/LoginActivity.kt
+++ app/src/main/java/com/example/wildifecare_mobile/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.example.wildifecare_mobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
@@ -12,5 +15,12 @@ class LoginActivity : AppCompatActivity() {
         var url : String? = bundle?.getString("url")
         val textView: TextView = findViewById(R.id.textView3) as TextView
         textView.text = url
+
+        val loginButton: Button = findViewById (R.id.loginButton)
+
+        loginButton.setOnClickListener() {
+            val i = Intent(this, MapsActivity::class.java)
+            startActivity(i);
+        }
     }
 }
